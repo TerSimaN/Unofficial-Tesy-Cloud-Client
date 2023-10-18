17eeb0e75c7105c81b52a3ac0eaa396dfbd9633b
TerSimaN <mcsiman998@gmail.com>
2023-10-19 01:08:27 +0300

    Removed unused classes

--- TesyContent/MqttResponseMessageContent/DeleteAllPrograms/AppDeleteAllProgramsContent.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-public record class AppDeleteAllProgramsContent (
-	[property: JsonPropertyName("code")] short Code,
-	[property: JsonPropertyName("message")] string Message,
-	[property: JsonPropertyName("app_id")] string AppId,
-	[property: JsonPropertyName("command")] string Command,
-	[property: JsonPropertyName("payload")] object Payload
-);
